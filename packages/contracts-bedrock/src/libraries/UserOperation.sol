40ebca31051adfd7085e54a08c6c3e5ea68faf5e
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-02 00:46:35 +0200

    contracts-bedrock: create library for UserOperations that uses memory
    instead of calldata

---
+++ packages/contracts-bedrock/src/libraries/UserOperation.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+import { UserOperation } from "account-abstraction/interfaces/UserOperation.sol";
+
+/**
+ * Utility functions helpful when working with UserOperation structs.
+ */
+library UserOperationLib {
+    function getSender(UserOperation memory userOp) internal pure returns (address) {
+        address data;
+        //read sender from userOp, which is first userOp member (saves 800 gas...)
+        assembly {data := calldataload(userOp)}
+        return address(uint160(data));
+    }
+
+    //relayer/block builder might submit the TX with higher priorityFee, but the user should not
+    // pay above what he signed for.
+    function gasPrice(UserOperation memory userOp) internal view returns (uint256) {
+    unchecked {
+        uint256 maxFeePerGas = userOp.maxFeePerGas;
+        uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
+        if (maxFeePerGas == maxPriorityFeePerGas) {
+            //legacy mode (for networks that don't support basefee opcode)
+            return maxFeePerGas;
+        }
+        return min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
+    }
+    }
+
+    function pack(UserOperation memory userOp) internal pure returns (bytes memory ret) {
+        address sender = getSender(userOp);
+        uint256 nonce = userOp.nonce;
+        bytes32 hashInitCode = keccak256(userOp.initCode);
+        bytes32 hashCallData = keccak256(userOp.callData);
+        uint256 callGasLimit = userOp.callGasLimit;
+        uint256 verificationGasLimit = userOp.verificationGasLimit;
+        uint256 preVerificationGas = userOp.preVerificationGas;
+        uint256 maxFeePerGas = userOp.maxFeePerGas;
+        uint256 maxPriorityFeePerGas = userOp.maxPriorityFeePerGas;
+        bytes32 hashPaymasterAndData = keccak256(userOp.paymasterAndData);
+
+        return abi.encode(
+            sender, nonce,
+            hashInitCode, hashCallData,
+            callGasLimit, verificationGasLimit, preVerificationGas,
+            maxFeePerGas, maxPriorityFeePerGas,
+            hashPaymasterAndData
+        );
+    }
+
+    function hash(UserOperation memory userOp) internal pure returns (bytes32) {
+        return keccak256(pack(userOp));
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
